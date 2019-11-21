5f40d14f84b0b1a3f95e2edf2eab96e2a100386a
rangpunzel <rangpunzel@gmail.com>
2019-11-21 18:27:51 +0900

    20191121

--- src/b_operator/ComparisonOperator.java
+++ src/b_operator/ComparisonOperator.java
@@ -42,7 +42,7 @@ public class ComparisonOperator {
 		b = true != false;
 		System.out.println(b);
 		
-	/*	b = "abc" == "abc";
+		b = "abc" == "abc";
 		System.out.println(b);  //같은 스트링 리터럴은 같은 주소를 갖는다.
 		b = "abc" == new String("abc"); //new는 새로운 주소를 생성한다.
 		System.out.println(b);
@@ -54,7 +54,25 @@ public class ComparisonOperator {
 		System.out.println(b);
 		b = !"abc".equals("ABC");
 		//같지 않다의 결과를 얻기 위해서는 !(NOT)을 앞에 붙여준다.
-		*/
+		
+		
+		//다음의 문장들을 코드로 작성 후 연산결과를 출력해주세요.
+		//1보다 2가 큰가?
+		b = 1 < 2;
+		System.out.println(b);
+		
+		//0은 100보다 작거나 같은가?
+		b = 0 <= 100;
+		System.out.println(b);
+		
+		//3.14f와 3.14는 다른가?
+		b = 3.14f != 3.14;
+		System.out.println(b);
+		
+		//"남자"와 "여자"는 다른가?
+		b = !"남자".equals("여자");
+		System.out.println(b);
+		
 		
 	}
 
